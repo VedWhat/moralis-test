63b38a94bd04ac4c336387aede41e3fe061d8b15
ved <ved.chitnis@gmail.com>
2022-05-23 12:53:16 +0530

    completed dapp

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
+import { MoralisProvider } from "react-moralis";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <MoralisProvider appId="kd2WU5Sivwq8X54JIWjMblBkFF9dxLl6lGC9fll9" serverUrl="https://caopm73h0q4e.usemoralis.com:2053/server">
+          <App />
+    </MoralisProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
